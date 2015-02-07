3533641c856c690763ba96b9e7b8dff9f14df644
Randy Nance <randynobx@gmail.com>
2015-02-06 22:39:21 -0500

    Fixed old var name in ../models/record.rb; Added Top Account stats on Dashboard; Updated tickets#show to link to records#new for ticket with no related records;

--- app/models/record.rb
+++ app/models/record.rb
@@ -7,7 +7,7 @@ class Record < ActiveRecord::Base
   validates :ticket_id, presence: true
   validates :income, presence: true, numericality: true
   validates :expense, presence: true, numericality: true
-  validates :balance, presence: true, numericality: true
+  validates :total, presence: true, numericality: true
   validates :notes, length: { maximum: 1000,
     			too_long: "%{count} characters is the maximum allowed" }
   end
